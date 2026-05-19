401e3502b6174017a790d09c334c3b6e7d0ba419
mkvasile <mkvasile@L16215>
2026-05-19 12:51:50 +0300

    markdown support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,26 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*.md",
+  callback = function()
+    vim.opt_local.textwidth = 80
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.bo.formatexpr = "" -- clear conform's hijack, use built-in
+    vim.cmd("normal! gggqG")
+    vim.api.nvim_win_set_cursor(0, { row, col })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,5 @@ vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.showbreak = "↪ "
 vim.opt.conceallevel = 0
+vim.opt_local.spelllang = "en,el"
+vim.opt_local.spelloptions = "camel"
