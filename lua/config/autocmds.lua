4c19310d6ec153e408b1ec5cdc8ef9743056317f
mkvasile <mkvasile@L16215>
2026-05-21 12:34:12 +0300

    remove markdown support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,26 +1,22 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.opt_local.textwidth = 80
-    vim.opt_local.wrap = true
-    vim.opt_local.linebreak = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*.md",
-  callback = function()
-    vim.opt_local.textwidth = 80
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    vim.bo.formatexpr = "" -- clear conform's hijack, use built-in
-    vim.cmd("normal! gggqG")
-    vim.api.nvim_win_set_cursor(0, { row, col })
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.opt_local.spell = false
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "markdown",
+--   callback = function()
+--     vim.opt_local.textwidth = 80
+--     vim.opt_local.wrap = true
+--     vim.opt_local.linebreak = true
+--     vim.opt_local.spell = false
+--     vim.bo.formatexpr = ""
+--     vim.opt_local.formatoptions:append("t")
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--   pattern = "*.md",
+--   callback = function()
+--     vim.opt_local.textwidth = 80
+--     vim.bo.formatexpr = ""
+--     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+--     vim.cmd("normal! gggqG")
+--     vim.api.nvim_win_set_cursor(0, { row, col })
+--   end,
+-- })
